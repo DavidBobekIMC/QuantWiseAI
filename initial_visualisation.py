fe985b0599943a858b59829919f21df64af8917b
DavidBobek <55330836+DavidBobek@users.noreply.github.com>
2023-07-15 13:55:37 +0200

    🎨data visualisation and data import

--- initial_visualisation.py
+++ initial_visualisation.py
@@ -16,3 +16,31 @@ def plot_candlestick_data_2(df):
                     close=df['close'])])
     fig.show()
   
+def plot_nasdaq(df,company,filename,covid):
+    fig = go.Figure(data=[go.Candlestick(x=df.index, open=df['Open'], high=df['High'], low=df['Low'], close=df['Close'])])
+    
+    #title
+    if covid:
+        fig.update_layout(
+            annotations=[dict(
+            x='2020-03-15', y=0.05, xref='x', yref='paper',
+            showarrow=False, xanchor='left', text='COVID-19 Crash Begins')],
+            shapes = [dict(
+            x0='2020-03-15', x1='2020-03-15', y0=0, y1=1, xref='x', yref='paper',
+            line_width=2)],
+    
+    )
+            
+    fig.update_layout(
+        title=
+        f"""Stock ticker:{company}\n 
+            year: {filename}\n
+            interval: daily \n
+            chart type: Candlestick chart""",
+            
+        yaxis_title='Price'
+    )
+    
+    
+    fig.show()
+    
--- main.py
+++ main.py
@@ -6,11 +6,10 @@ import matplotlib.pyplot as plt
 import datetime
 import seaborn as sns
 import plotly.graph_objects as go
-
+import os
 import matplotlib.pyplot as plt
 import pandas as pd
-from initial_visualisation import plot_candlestick_data, plot_candlestick_data_2
-
+from initial_visualisation import plot_candlestick_data, plot_candlestick_data_2, plot_nasdaq
 
 def S_and_P_500():
     data = pd.read_csv("data\S&P500.csv")
@@ -49,7 +48,63 @@ def google_data():
         x = df['timestamp']
         
         plot_candlestick_data_2(df)
+        
+def nasdaq_data():
+
+    folder_path = 'data_nasdaq'  # Replace with the actual folder path
+
+    # Iterate over all files in the folder
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        
+        # Check if the current item is a file
+        if os.path.isfile(file_path):
+            # Process the file or perform desired operations
+            data = pd.read_csv(file_path)
+            
+            
+            df = pd.DataFrame(data, columns=['Date', 'Close/Last', 'Volume', 'Open', 'High', 'Low'])
+        
+            # Format the Date column as datetime
+            df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y')
+            df.set_index('Date', inplace=True)
+
+            # Rename the column for compatibility with plot_candlestick_data function
+            df.rename(columns={'Close/Last': 'Close'}, inplace=True)
+            
+            
+            #plot the data
+            plot_nasdaq(df,filename)
+            
+def plot_yearly(filename):
+    data = pd.read_csv(filename)
+            
+            
+    df = pd.DataFrame(data, columns=['Date', 'Close/Last', 'Volume', 'Open', 'High', 'Low'])
+
+    # Format the Date column as datetime
+    df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y')
+    df.set_index('Date', inplace=True)
+
+    # Rename the column for compatibility with plot_candlestick_data function
+    df.rename(columns={'Close/Last': 'Close'}, inplace=True)
+    
+    company = filename.split("Data_")[1].split(".")[0]
+    data_by_year = {}
+    for entry in df.index:
+        year = entry.year
+        if year not in data_by_year:
+            data_by_year[year] = []
+        data_by_year[year].append(entry)
+    
+    for year in data_by_year:
+        plot_nasdaq(df.loc[data_by_year[year]],company=company,filename=year,covid=False)
+    
+
+        
     
 if __name__ == "__main__":
-    S_and_P_500()
-    google_data()
+    #S_and_P_500()
+    #google_data()
+    #nasdaq_data()
+    plot_yearly("data_nasdaq\HistoricalData_SBUX.csv")
